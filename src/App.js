b41f949f4e0f043d7d319d8f57c2c21b2113c768
MajaDit <marie.ditrtova@gmail.com>
2021-07-13 13:47:34 +0100

    added styling

--- src/App.js
+++ src/App.js
@@ -15,64 +15,66 @@ function App() {
   };
   return (
     <div className="App">
-      <div className="card">
-        <div className="card-body">
-          <form className="search-form">
-            <input
-              type="text"
-              placeholder=" Find City"
-              className="input"
-              autoFocus="on"
-            />
-            <input type="submit" value="Search" className="submit-button" />
-          </form>
-          <h1 className="city">
-            {weatherData.city}, {weatherData.country}
-          </h1>
-          <div className="row">
-            <div className="col-6">
-              <div className="day-and-time">
-                Last updated:
-                <span>
-                  {weatherData.day} {weatherData.time}
-                </span>
-              </div>
-              <div className="row">
-                <div className=" col-3 icon">
-                  <img src={weatherData.imgUrl} alt="icons" />
-                </div>
-                <div className="col">
-                  <span className="col-7 current-temperature">
-                    {weatherData.temperature}
+      <div className="container">
+        <div className="card">
+          <div className="card-body">
+            <form className="search-form">
+              <input
+                type="text"
+                placeholder=" Find City"
+                className="input"
+                autoFocus="on"
+              />
+              <input type="submit" value="Search" className="submit-button" />
+            </form>
+            <h1 className="city">
+              {weatherData.city}, {weatherData.country}
+            </h1>
+            <div className="row">
+              <div className="col-6">
+                <div className="day-and-time">
+                  Last updated:
+                  <span>
+                    {weatherData.day} {weatherData.time}
                   </span>
-                  <span className="units"> °C </span>
+                </div>
+                <div className="row">
+                  <div className=" col-3 icon">
+                    <img src={weatherData.imgUrl} alt="icons" />
+                  </div>
+                  <div className="col">
+                    <span className="col-7 current-temperature">
+                      {weatherData.temperature}
+                    </span>
+                    <span className="units"> °C </span>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="col-5">
-              <ul>
-                <li className="description">{weatherData.description}</li>
-                <li className="humidity">
-                  Humidity:{" "}
-                  <span className="humidity">{weatherData.humidity}%</span>
-                </li>
-                <li className="wind">
-                  Wind: <span>{weatherData.wind}mph</span>
-                </li>
-              </ul>
+              <div className="col-6">
+                <ul>
+                  <li className="description">{weatherData.description}</li>
+                  <li className="humidity">
+                    Humidity:{" "}
+                    <span className="humidity">{weatherData.humidity}%</span>
+                  </li>
+                  <li className="wind">
+                    Wind: <span>{weatherData.wind}mph</span>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="credit">
-        <a
-          href="https://github.com/MajaDit/react-project"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-source code
-        </a>{" "}
-        by <strong>Maja Ditrtova</strong>.
+        <div className="credit">
+          <a
+            href="https://github.com/MajaDit/react-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code
+          </a>{" "}
+          by <strong>Maja Ditrtova</strong>.
+        </div>
       </div>
     </div>
   );
