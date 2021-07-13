00082758c912fab63d8ce22ddbfca361da13bc09
MajaDit <marie.ditrtova@gmail.com>
2021-07-13 15:31:19 +0100

    set up api

--- src/App.js
+++ src/App.js
@@ -1,83 +1,91 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+import React, { useState } from "react";
 
-function App() {
-  let weatherData = {
-    city: "London",
-    country: "UK",
-    day: "Tuesday",
-    time: "14:00",
-    imgUrl: "https://img.icons8.com/doodle/50/000000/moon-and-sun.png",
-    temperature: 20,
-    description: "Sunny",
-    humidity: 50,
-    wind: 20,
-  };
-  return (
-    <div className="App">
-      <div className="container">
-        <div className="card">
-          <div className="card-body">
-            <form className="search-form">
-              <input
-                type="text"
-                placeholder=" Find City"
-                className="input"
-                autoFocus="on"
-              />
-              <input type="submit" value="Search" className="submit-button" />
-            </form>
-            <h1 className="city">
-              {weatherData.city}, {weatherData.country}
-            </h1>
-            <div className="row">
-              <div className="col-6">
-                <div className="day-and-time">
-                  Last updated:
-                  <span>
-                    {weatherData.day} {weatherData.time}
-                  </span>
-                </div>
-                <div className="row">
-                  <div className=" col-3 icon">
-                    <img src={weatherData.imgUrl} alt="icons" />
+export default function App(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    });
+  }
+  if (weatherData.ready) {
+    return (
+      <div className="App">
+        <div className="container">
+          <div className="card">
+            <div className="card-body">
+              <form className="search-form">
+                <input
+                  type="text"
+                  placeholder=" Find City"
+                  className="input"
+                  autoFocus="on"
+                />
+                <input type="submit" value="Search" className="submit-button" />
+              </form>
+              <h1 className="city">{weatherData.city}, UK</h1>
+              <div className="row">
+                <div className="col-6">
+                  <div className="day-and-time">
+                    Last updated:
+                    <span>Tuesday 14:00</span>
                   </div>
-                  <div className="col">
-                    <span className="col-7 current-temperature">
-                      {weatherData.temperature}
-                    </span>
-                    <span className="units"> °C </span>
+                  <div className="row">
+                    <div className=" col-3 icon">
+                      <img
+                        src={weatherData.icon}
+                        alt={weatherData.description}
+                      />
+                    </div>
+                    <div className="col">
+                      <span className="col-7 current-temperature">
+                        {weatherData.temperature}
+                      </span>
+                      <span className="units"> °C </span>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="col-6">
-                <ul>
-                  <li className="description">{weatherData.description}</li>
-                  <li className="humidity">
-                    Humidity:{" "}
-                    <span className="humidity">{weatherData.humidity}%</span>
-                  </li>
-                  <li className="wind">
-                    Wind: <span>{weatherData.wind}mph</span>
-                  </li>
-                </ul>
+                <div className="col-6">
+                  <ul>
+                    <li className="description">{weatherData.description}</li>
+                    <li className="humidity">
+                      Humidity:{" "}
+                      <span className="humidity">{weatherData.humidity}%</span>
+                    </li>
+                    <li className="wind">
+                      Wind: <span>{weatherData.wind}mph</span>
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="credit">
-          <a
-            href="https://github.com/MajaDit/react-project"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Open-source code
-          </a>{" "}
-          by <strong>Maja Ditrtova</strong>.
+          <div className="credit">
+            <a
+              href="https://github.com/MajaDit/react-project"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Open-source code
+            </a>{" "}
+            by <strong>Maja Ditrtova</strong>.
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  } else {
+    let apiUrl = `api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=ad2ff494a03279abf350d133542944f6&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
 
-export default App;
+    return "Loading...";
+  }
+}
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App defaultCity="london" />
   </React.StrictMode>,
   document.getElementById("root")
 );
